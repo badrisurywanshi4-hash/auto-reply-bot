b0b0f828061ecbad02c978c0b51693c1c39e20ac
badrisurywanshi4-hash <badrisurywanshi4@gmail.com>
2026-05-18 21:19:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-
 import os
 import asyncio
-import google.generativeai as genai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from google import genai
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=GEMINI_API_KEY)
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
+    try:
+        user_message = update.message.text
 
-    response = model.generate_content(user_message)
+        response = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=user_message
+        )
 
-    await update.message.reply_text(response.text)
+        await update.message.reply_text(response.text)
+
+    except Exception as e:
+        await update.message.reply_text("Error: AI not working")
 
 async def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(MessageHandler(filters.TEXT, reply))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-    print("AI Bot is running...")
+    print("Bot is running...")
 
     await app.initialize()
     await app.start()
